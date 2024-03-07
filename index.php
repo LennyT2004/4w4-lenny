ce8730945f5f70ba4ecf3f931ff6bc7a1d25d2fc
Tran <e2269994@cmaisonneuve.qc.ca>
2024-03-07 10:09:46 -0500

    s5c2 Ajout des nouveaux liens dans le menu, ajout de header.php et vague.php

--- index.php
+++ index.php
@@ -9,14 +9,10 @@
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Protest+Guerrilla&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap" rel="stylesheet">
 
 <body>
-	<nav id="menu" class="global">
-		<header class="menu_header">
-			<?php wp_nav_menu(array("container" => "nav")); ?>
-		</header>
-	</nav>
+	<?php get_header(); ?>
 	<div id="entete" class="global">
 		<section class="entete_header">
 			<h1>Thème du groupe #1 (h1)</h1>
@@ -83,11 +79,7 @@
 				architecto nesciunt odit quos recusandae distinctio?
 			</p>
 		</section>
-		<div class="vague">
-			<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-				<path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" style="fill: var(--couleur-arriere-footer);"></path>
-			</svg>
-		</div>
+		<!-- Vague -->
 	</div>
 	<div id="footer" class="global">
 		<footer>
--- readme.md
+++ readme.md
@@ -1,6 +1,26 @@
 # Labo #4
 
 ## Création d'un menu
+- Ajouter un menu permettant d'accéder
+ - À vos cinq cours préférés
+ - À chacune des sections de la page
+- Le menu doit être formaté en utilisant composant/_menu.scss Le menu doit être adaptatif et être amovible en version mobile.
+
+### Création d'un header.php
+- Créer le template header.php qui contiendra la structure d'entête de la page qui sera reproduite dans chacun des modules de base
+
+### Création d'un « template-part » (gabarit)
+- Créer un template-part qui permettra d'afficher les vagues
+
+## Barème de correction
+- Intégration de chacun des éléments précédents de façon structuré (1 point)
+- Création d'une github-page à partir de index.htm
+- Création du readme.md avec :
+ - Le nom de l'auteur
+ - Un résumé des étapes du labo-4
+ - Un lien vers votre github-page
+ - Production de 5 commits
+- Design général de la page (1 point)
 
 ### Lien page github
 https://lennyt2004.github.io/4w4-lenny/
